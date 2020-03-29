e391f828172d1d095eb4bc9e647dc1c37c1a9c9a
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2020-03-29 20:13:47 +0200

    Implement two screens and navigate between them

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+
+const HomeScreen = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app111!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => props.navigation.navigate('Details')}
+      />
     </View>
   );
+}
+
+const DetailsScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
+
+
+export default function App() {
+  return <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
 }
 
 const styles = StyleSheet.create({
